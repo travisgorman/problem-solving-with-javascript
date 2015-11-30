bc787bc704573acf6d2d05073ce090ae55c7038d
Travis Gorman <thefarbetween@me.com>
2015-11-29 21:33:54 -0600

    Delete LetterCap-withMap.js

--- LetterCap-withMap.js
+++
@@ -1,9 +0,0 @@
-function LetterCapitalize(str){
-	return str.split(' ')
-        	.map(word => word
-        		.charAt(0)
-        			.toUpperCase() + word
-        				.slice(1))
-									.join(' ');
-}
-
