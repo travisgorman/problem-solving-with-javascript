821d8f676730d2d3c2a706eedc9b111b730b75e3
Travis Gorman <thefarbetween@me.com>
2015-11-29 21:36:16 -0600

    Create LetterCap-withMap.js

---
+++ LetterCap-withMap.js
@@ -0,0 +1,8 @@
+function LetterCapitalize(str){
+	return str.split(' ')
+      .map(word => word
+        .charAt(0)
+          .toUpperCase() + word
+            .slice(1))
+	            .join(' ');
+}
