7778f176942ddd79a54f4795bdcc3e8dd63ab471
Travis Gorman <thefarbetween@me.com>
2015-11-29 21:37:55 -0600

    fix indentation

--- LetterCap-withMap.js
+++ LetterCap-withMap.js
@@ -1,8 +1,8 @@
 function LetterCapitalize(str){
-	return str.split(' ')
-      .map(word => word
-        .charAt(0)
-          .toUpperCase() + word
-            .slice(1))
-	            .join(' ');
+  return str.split(' ')
+    .map(word => word
+      .charAt(0)
+        .toUpperCase() + word
+          .slice(1))
+	          .join(' ');
 }
